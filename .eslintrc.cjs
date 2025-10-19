deee92813f94e2812b03a377d9e542477e717076
olegin77 <82461956+olegin77@users.noreply.github.com>
2025-10-19 13:13:03 +0500

    Update and rename eslintrc.cjs to .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,18 @@
 module.exports = {
   root: true,
   env: { es2023: true, node: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:security/recommended-legacy"
-  ],
+  extends: ["eslint:recommended", "plugin:security/recommended-legacy"],
   plugins: ["security"],
   parserOptions: { ecmaVersion: 2023, sourceType: "module" },
   rules: {
     "no-console": "off",
     "security/detect-object-injection": "off"
-  }
+  },
+  ignorePatterns: [
+    "node_modules/",
+    "dist/",
+    "build/",
+    ".next/",
+    "coverage/"
+  ]
 };
